e30b4454404b34313b81f70261bdf5ee7e32b64f
shakil hasan <shshakilhasan69@gmail.com.com>
2021-04-17 12:05:05 -0700

    redme file edite

--- src/Components/Home/Home/TestimonialInfo/TestimonialInfo.js
+++ src/Components/Home/Home/TestimonialInfo/TestimonialInfo.js
@@ -3,7 +3,7 @@ import React from 'react';
 const TestimonialInfo = (props) => {
     const {review}=props.testi;
     return (
-        <div className="col-md-4">
+        <div className="col-md-4 col-sm-12 mt-5">
             <div><h3>{review.heardwor}</h3></div>
             <div><h5>{review.messeg}</h5></div>
             <div><h4>--{review.name}</h4></div>
